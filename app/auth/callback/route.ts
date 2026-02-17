f94f13650ad0a068321466dbcbe0301757cb9b07
yuxin <yuxinh402@gmail.com>
2026-02-16 22:56:58 -0500

    adjusted page

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,17 +6,17 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
 
-
-  const next = '/'
-
   if (code) {
-    const cookieStore = cookies()
+    const cookieStore = await cookies()
+
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
-          get(name: string) { return cookieStore.get(name)?.value },
+          get(name: string) {
+            return cookieStore.get(name)?.value
+          },
           set(name: string, value: string, options: any) {
             cookieStore.set({ name, value, ...options })
           },
@@ -27,14 +27,12 @@ export async function GET(request: Request) {
       }
     )
 
-
     const { error } = await supabase.auth.exchangeCodeForSession(code)
 
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
+      return NextResponse.redirect(`${origin}/`)
     }
   }
-
 
   return NextResponse.redirect(`${origin}`)
 }
